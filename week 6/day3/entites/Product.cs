04960cf413a650a0cba0d5e210db9b30753df3f2
jeevan0894 <jeevanpogiri1@gmail.com>
2026-03-26 16:06:12 +0530

    Add files via upload

---
+++ week 6/day3/entites/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace week6handson.day3.entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Category {  get; set; }
+        public double price { get; set; }
+    }
+}
