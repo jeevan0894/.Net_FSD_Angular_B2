34dc011e19962d361714a3c0fcd439bc14895952
jeevan0894 <jeevanpogiri1@gmail.com>
2026-04-27 13:24:15 +0530

    Update p4.cs

--- week5/day1/p4.cs
+++ week5/day1/p4.cs
@@ -1,1 +1,47 @@
 
+﻿using System;
+
+namespace hands_on_prblm_week5_day1
+{
+    class Vehicle
+    {
+        public string Brand { get; set; }
+        public double RentalRatePerDay { get; set; }
+
+        public virtual double CalculateRental(int days)
+        {
+            return RentalRatePerDay * days;
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public override double CalculateRental(int days)
+        {
+            double total = RentalRatePerDay * days;
+            return total + 500;
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        public override double CalculateRental(int days)
+        {
+            double total = RentalRatePerDay * days;
+            return total - (total * 0.05);
+        }
+    }
+
+    class P4
+    {
+        static void Main()
+        {
+            Vehicle car = new Car();
+            car.RentalRatePerDay = 2000;
+
+            int days = 3;
+
+            Console.WriteLine("Total Rental = " + car.CalculateRental(days));
+        }
+    }
+}
