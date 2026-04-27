acc2d5cd0e678407348a74a089084b2af8f6618b
jeevan0894 <jeevanpogiri1@gmail.com>
2026-04-27 13:20:53 +0530

    Update p2.cs

--- week5/p2.cs
+++ week5/p2.cs
@@ -1,1 +1,47 @@
 
+﻿using System;
+
+namespace hands_on_prblm_week5_day1
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public double BaseSalary { get; set; }
+
+        public virtual double CalculateSalary()
+        {
+            return BaseSalary;
+        }
+    }
+
+    class Manager : Employee
+    {
+        public override double CalculateSalary()
+        {
+            return BaseSalary + (BaseSalary * 0.20);
+        }
+    }
+
+    class Developer : Employee
+    {
+        public override double CalculateSalary()
+        {
+            return BaseSalary + (BaseSalary * 0.10);
+        }
+    }
+
+    class P2
+    {
+        static void Main()
+        {
+            Employee manager = new Manager();
+            manager.BaseSalary = 50000;
+
+            Employee developer = new Developer();
+            developer.BaseSalary = 50000;
+
+            Console.WriteLine("Manager Salary = " + manager.CalculateSalary());
+            Console.WriteLine("Developer Salary = " + developer.CalculateSalary());
+        }
+    }
+}
